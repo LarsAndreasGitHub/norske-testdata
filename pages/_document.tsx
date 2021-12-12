b1eba8dfc36b660ec1ce64b39bd36eb132efea9b
LarsAndreasGitHub <lars.tveiten@bekk.no>
2021-12-12 23:18:10 +0100

    Add adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,11 @@ class MyDocument extends Document {
                             `,
                         }}
                     />
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4866287046383266"
+                        crossOrigin="anonymous"
+                    />
                 </Head>
                 <body>
                     <Main />
