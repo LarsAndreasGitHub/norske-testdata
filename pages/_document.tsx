655e6f679152dc1f21b7110414da9d853dae366b
LarsAndreasGitHub <lars.tveiten@bekk.no>
2021-12-15 16:34:42 +0100

    Add hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,20 @@ class MyDocument extends Document {
                             `,
                         }}
                     />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                (function(h,o,t,j,a,r){
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:2749544,hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                            `,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
