36324b5267f44f9d45083dbde2d2c305416ff08b
LarsAndreasGitHub <lars.tveiten@bekk.no>
2022-10-29 10:46:35 +0200

    Update fiktivt fnr intro text

--- components/FiktivFnrPage/FiktivtFnrText/FiktivtFnrIntroText.tsx
+++ components/FiktivFnrPage/FiktivtFnrText/FiktivtFnrIntroText.tsx
@@ -4,36 +4,16 @@ import linkStyle from '../../common/link/link.module.scss';
 export const FiktivtFnrIntroText: FunctionComponent = () => (
     <>
         <p>
-            Her kan du lage organisasjonsnumre til bruk som testdata. Numrene genereres tilfeldig, og følger reglene for
-            oppbygging av organisasjonsnumre{' '}
+            Fiktive fødselsnumre er ikke ekte fødselsnumre. En vanlig måte å lage fiktive fødselsnumre på er å{' '}
             <a
+                href="https://www.skatteetaten.no/person/folkeregister/fodsel-og-navnevalg/barn-fodt-i-norge/fodselsnummer/"
                 className={linkStyle.link}
-                href="https://www.brreg.no/om-oss/oppgavene-vare/alle-registrene-vare/om-enhetsregisteret/organisasjonsnummeret/"
             >
-                beskrevet hos Brønnøysundregistrene
+                følge oppbyggingen til vanlige fødselsnumre
             </a>
-            .
-        </p>
-        <p>
-            Hvis spesifisert, gjør vi kall mot Brønnøysundregistrene sitt{' '}
-            <a href="https://data.brreg.no/enhetsregisteret/api/docs/index.html" className={linkStyle.link}>
-                API
-            </a>{' '}
-            for å garantere at organisasjonsnummeret ikke er i bruk hos noen registrerte{' '}
-            <a
-                href="https://data.brreg.no/enhetsregisteret/api/docs/index.html#enheter-oppslag"
-                className={linkStyle.link}
-            >
-                enheter
-            </a>{' '}
-            eller{' '}
-            <a
-                href="https://data.brreg.no/enhetsregisteret/api/docs/index.html#underenheter-oppslag"
-                className={linkStyle.link}
-            >
-                underenheter
-            </a>
-            .
+            , men å legge til et tall i fødselsdatoen slik at den ikke lenger er en gyldig dato. Slike fødselsnummere
+            vil bestå fødselsnummervalidering som bare sjekker kontrollsifre, men ikke de som kontrollerer dato. Pass på
+            at du velger riktig oppbygging til bruk i din virksomhet.
         </p>
     </>
 );
